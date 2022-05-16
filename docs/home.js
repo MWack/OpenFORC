a1193b83537dd2cc842330fe6ff593c464993c8f
Michael <wack@geophysik.uni-muenchen.de>
2022-05-16 17:02:03 +0200

    remover forc stuff. its merged into munmagtools

--- docs/home.js
+++
@@ -1,9 +0,0 @@
-const template = `<h1>Welcome to my toolbox.</h1>
-    <div>Please use the navigation bar on top to access different tools.</div>
-    <div>created by Michael Wack 2021-2022</div>`
-
-export default {
-  template,
-  data () {return {}},
-  computed: {},
-}
